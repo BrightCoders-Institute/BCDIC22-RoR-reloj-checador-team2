b3f2ea4b52451e0815b5b15a4fdac39747c21ef8
Everardo Alcaraz <72171554+EverardoAlcarazBaltazar@users.noreply.github.com>
2023-01-13 17:36:17 -0600

    login admin layout added
    Co-authored-by: DiegoMoralesBravo <diegomorales1359@gmail.com>
    Co-authored-by: ikerismak <ikerismak@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/cheking_employee',  to: "main#cheking_employee"
   get '/register_employee', to: "main#register_employee"
   get '/register_store', to: "main#register_store"
+  get '/login_admin', to: "main#login_admin"
 
 end
