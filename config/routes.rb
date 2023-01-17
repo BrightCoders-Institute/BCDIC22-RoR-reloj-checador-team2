bc9af9be6db4f0e78db96bf1d1e15b8787c822c9
Everardo Alcaraz <72171554+EverardoAlcarazBaltazar@users.noreply.github.com>
2023-01-17 17:44:13 -0600

    employee and store controllers added
    Co-authored-by: DiegoMoralesBravo <diegomorales1359@gmail.com>
    Co-authored-by: ikerismak <ikerismak@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root  "main#checking_employee"
   get '/checking_employee',  to: "main#checking_employee"
-  get '/register_employee', to: "main#register_employee"
-  get '/register_store', to: "main#register_store"
   get '/login_admin', to: "main#login_admin"
-  get '/crud_employees', to: "main#crud_employees"
-  get '/crud_store', to: "main#crud_store"
+  get '/register_employee', to: "employee#register_employee"
+  get '/crud_employees', to: "employee#crud_employees"
+  get '/register_store', to: "store#register_store"
+  get '/crud_store', to: "store#crud_store"
 
 end
