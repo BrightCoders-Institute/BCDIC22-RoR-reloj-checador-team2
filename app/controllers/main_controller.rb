3ad5861b3b218909256fcfdc28c3beebd4ff7efc
Everardo Alcaraz <72171554+EverardoAlcarazBaltazar@users.noreply.github.com>
2023-01-17 17:44:13 -0600

    employee and store controllers added
    Co-authored-by: DiegoMoralesBravo <diegomorales1359@gmail.com>
    Co-authored-by: ikerismak <ikerismak@gmail.com>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,17 +1,6 @@
 class MainController < ApplicationController
+  
   def checking_employee
-  end
-
-  def register_employee
-  end
-
-  def register_store
-  end
-
-  def crud_employees
-  end
-
-  def crud_store
   end
 
   def login_admin
