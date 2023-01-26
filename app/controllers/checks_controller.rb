faff5b8d60d475a6213123c7eb84c6dd8a6d9200
DiegoMoralesBravo <diegomorales1359@gmail.com>
2023-01-26 16:06:34 -0600

    Model and migrate Check created, database connection to check-in-out form
    Co-authored-by: ikerismak <ikerismak@gmail.com>
    Co-authored-by: EverardoAlcarazBaltazar <ealcaraz12@ucol.mx>

---
+++ app/controllers/checks_controller.rb
@@ -0,0 +1,26 @@
+class ChecksController < ApplicationController
+    def index
+    end
+  
+    def create
+      @employee = Employee.find_by(employee_params)
+      unless @employee.nil?
+        @check = @employee.checks.new(check_params.merge(:datetime => Time.now.strftime('%H:%M - %F')))
+        @check.save ? flash[:notice] = 'Successfully check' : flash[:alert] = 'Checking faill'
+      else
+        flash[:alert] = 'Wrong number or password'
+      end
+      redirect_back(fallback_location: root_path)
+    end
+  
+    private
+    
+    def employee_params
+      params.permit(:employee, :number_private)
+    end
+  
+    def check_params
+      params.permit(:check)
+    end
+  end
+  
