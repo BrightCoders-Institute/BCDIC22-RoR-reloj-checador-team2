195986e4d6bec4a967a1cca4341dbe875ed4e46e
DiegoMoralesBravo <diegomorales1359@gmail.com>
2023-01-26 15:16:16 -0600

    Refactor in edit and new form
    Co-authored-by: ikerismak <ikerismak@gmail.com>
    Co-authored-by: EverardoAlcarazBaltazar <ealcaraz12@ucol.mx>

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,49 +1,51 @@
 class StoresController < ApplicationController
-    def index
-      @stores = Store.all
-    end
+  def index
+    @stores = Store.all
+  end
 
-    def show
-      @store = Store.find(params[:id])
-    end
+  def show
+    set_store
+  end
 
-    def new
-      @store = Store.new
-    end
-  
-    def create
-      @store = Store.new(store_params)
-  
-      if @store.save
-        redirect_to "/stores"
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
+  def new
+    @store = Store.new
+  end
 
-    def edit
-      @store = Store.find(params[:id])
+  def create
+    @store = Store.new(store_params)
+    if @store.save
+      redirect_to '/stores'
+    else
+      render :new, status: :unprocessable_entity
     end
-  
-    def update
-      @store = Store.find(params[:id])
-  
-      if @store.update(store_params)
-        redirect_to '/stores'
-      else
-        render :edit, status: :unprocessable_entity
-      end
+  end
+
+  def edit
+    set_store
+  end
+
+  def update
+    set_store
+    if @store.update(store_params)
+      redirect_to '/stores'
+    else
+      render :edit, status: :unprocessable_entity
     end
-  
-    def destroy
-      @store = Store.find(params[:id])
-      @store.destroy
-  
-      redirect_to stores_path, status: :see_other
-    end
-  
-    private
-      def store_params
-        params.require(:store).permit(:name, :adress)
-      end
+  end
+
+  def destroy
+    set_store
+    @store.destroy
+    redirect_to stores_path, status: :see_other
+  end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name, :adress)
+  end
+
+  def set_store
+    @store = Store.find(params[:id])
+  end
 end
