fb79a9883842eed25a1e216c8a503c3dd257556d
DiegoMoralesBravo <diegomorales1359@gmail.com>
2023-02-03 17:07:52 -0600

    Fix bug

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -7,7 +7,7 @@ class ChecksController < ApplicationController
       flash[:alert] = 'Wrong number or password'
     else
       @check = Check.new(check_params.merge(timestamp: Time.now.strftime('%H:%M - %F'),
-                                            id_employee: @employee.id))
+                                            id_employee: @employee.id, id_sucursal: @employee.id_store))
       @check.save ? flash[:notice] = 'Successfully check' : flash[:alert] = 'Checking faill'
     end
     redirect_back(fallback_location: root_path)
